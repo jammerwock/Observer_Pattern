c07010ffbee8adad7c74178367d1e5e6c34f3bb2
Aizen <aizen@meta.ua>
2015-03-24 17:08:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+// http://www.javenue.info/post/76
+
+import java.util.ArrayList;
+
+interface IObserver {
+    void created(Object obj);
+
+    void modified(Object obj);
+}
+
+class DefaultObserver implements IObserver {
+
+    @Override
+    public void created(Object obj) {
+        System.out.println("DefaultObserver::created() for " + obj);
+    }
+
+    @Override
+    public void modified(Object obj) {
+        System.out.println("DefaultObserver::modified() for " + obj);
+    }
+}
+
+class MyObserver implements IObserver {
+
+    @Override
+    public void created(Object obj) {
+        System.out.println("MyObserver::created() for " + obj);
+    }
+
+    @Override
+    public void modified(Object obj) {
+        System.out.println("MyObserver::modified() for " + obj);
+    }
+}
+
+
+class Observers<T extends IObserver> extends ArrayList<T> {
+    public void notifyCreated(Object obj) {
+        for (T obs : Observers.this) {
+            obs.created(obj);
+        }
+    }
+
+    public void notifyModified(Object obj) {
+        for (T obs : Observers.this) {
+            obs.modified(obj);
+        }
+    }
+}
+
+public class Main {
+    Observers obs = new Observers();
+
+    @Override
+    public String toString() {
+        return "Main";
+    }
+
+
+    Main() {
+        obs.add(new DefaultObserver());
+        obs.add(new MyObserver());
+        obs.notifyCreated(this);
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.t();
+    }
+
+    void t() {
+        obs.notifyModified(this);
+    }
+
+
+}
+
